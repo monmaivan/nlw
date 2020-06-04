4460b7d9f97b3f16d250243ae12f68ff901c4b9e
Ivan Monma <monma.ivan@outlook.com>
2020-06-04 08:01:00 -0300

    Final Dia 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,65 +1,69 @@
-function populateUFs() {
-    const uFSelect = document.querySelector("select[name=uf]")
-    
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( (res) => res.json() )
-    /* .then( (res) => { return res.json() }) */
-    .then( states => {
-
-        for( const state of states) {
-            uFSelect.innerHTML += `<option value ="${state.id}">${state.nome}</option>`
-            /*uFSelect.innerHTML = uFSelect.innerHTML + `<option value ="1">Valor</}option>`*/
-        }
-    } )
+function ordenaPorNome(x,y) {
+    return ((x.nome == y.nome) ? 0 : ((x.nome > y.nome) ? 1 : -1 ));
 }
 
-populateUFs()
+function populateUFs() {
+    const ufSelect = document.querySelector("[name=uf]");
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then( res => res.json())
+        .then( states => {
+            states.sort(ordenaPorNome);
+            for( state of states) {
+                ufSelect.innerHTML += '<option value="'+state.id+'">'+state.nome+'</option>';
+            }
+            
+        })
+}
+
+
+
+populateUFs();
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=state]")
-
-    const ufValue  = event.target.value
-
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState]
-
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
-    citySelect.disabled = true
-
-    fetch(url)
-    .then( (res) => res.json() ) /* .then( (res) => { return res.json() }) */
-    .then( cities => {
-
-        for( const city of cities) {
-            citySelect.innerHTML += `<option value ="${city.nome}">${city.nome}</option>` /*uFSelect.innerHTML = uFSelect.innerHTML + `<option value ="1">Valor</}option>`*/
-        }
-        citySelect.disabled = false
-    } )
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("[name=state]");
+    citySelect.innerHTML="<option value>Selecione a cidade</option>";
+    citySelect.disabled = true;
+    stateInput.value = event.target.options[event.target.selectedIndex].text;
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+event.target.value+"/municipios")
+        .then( res => res.json())
+        .then( cities => {
+            for( city of cities) {
+                citySelect.innerHTML += '<option value="'+city.nome+'">'+city.nome+'</option>';
+            }
+            citySelect.disabled = false;
+        })
 }
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+    .addEventListener("change", getCities);
 
-
-// items de coleta
-// pegar todos os li´s
-
-const itemsToCollect = document.querySelectorAll(".items-grid li")
+// Itens de coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li");
 
 for (const item of itemsToCollect) {
-    item.addEventListener("click", handleSelectedItem)
+    item.addEventListener("click", handleSelectedItem);
 }
 
+// array com itens selecionados
+let selectedItems = [];
+
+//input hidden com valores dos itens selecionados
+const collectedItems = document.querySelector("input[name=items]");
+
 function handleSelectedItem(event) {
-    const itemLi = event.target
+    const itemLi = event.target;
+    const itemId = itemLi.dataset.id;
+    itemLi.classList.toggle("selected");
+    addOrRemove(selectedItems, itemId);
 
-    //adicionar ou remover uma classe com js
-    itemLi.classList.add.toggle("selected")
-    
-    const itemId = itemLi.dataset.id
+    // alimenta o input hidden com os valores atuais do array
+    collectedItems.value = selectedItems;
+}
 
+// função para adicionar item no array se não existir ou remover se existir
+function addOrRemove(array, value) {
+    var index = array.indexOf(value);
+    index === -1 ? array.push(value) : array.splice(index, 1);
 }
